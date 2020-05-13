ec0a71e2f2875f3fe2d6b50dc147c3c8a96e0560
mja00 <me@mja00.dev>
2020-05-13 02:22:12 -0400

    Added a date obj to the backup

--- src/main/java/dev/mja00/fuckthewhitelist/FuckTheWhitelist.java
+++ src/main/java/dev/mja00/fuckthewhitelist/FuckTheWhitelist.java
@@ -2,16 +2,14 @@ package dev.mja00.fuckthewhitelist;
 
 import org.bukkit.OfflinePlayer;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Set;
-import java.util.UUID;
 
 public final class FuckTheWhitelist extends JavaPlugin {
 
@@ -19,6 +17,30 @@ public final class FuckTheWhitelist extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getLogger().info("Generating backup whitelist.");
+        Set<OfflinePlayer> whitelisted = getServer().getWhitelistedPlayers();
+        ArrayList<String> whitelist = new ArrayList<String>();
+        JSONObject players = new JSONObject();
+        for (OfflinePlayer player : whitelisted) {
+            String mcuuid = player.getUniqueId().toString();
+            String name = player.getName();
+            players.put("uuid", mcuuid);
+            players.put("name", name);
+            //getLogger().info(players.toString());
+            whitelist.add(players.toJSONString());
+            players.clear();
+        }
+        getLogger().info("Writing whitelist to file.");
+        generateWhitelist(whitelist);
+        getLogger().info("Whitelist backed up.");
+
+    }
+
+    @lombok.SneakyThrows
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        getLogger().info("Generating backup whitelist.");
         Set<OfflinePlayer> whitelisted = getServer().getWhitelistedPlayers();
         ArrayList<String> whitelist = new ArrayList<String>();
         JSONObject players = new JSONObject();
@@ -31,18 +53,14 @@ public final class FuckTheWhitelist extends JavaPlugin {
             whitelist.add(players.toJSONString());
             players.clear();
         }
+        getLogger().info("Writing whitelist to file.");
         generateWhitelist(whitelist);
-
-
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("Whitelist backed up.");
     }
 
     public static void generateWhitelist(ArrayList<String> whitelist) throws IOException{
-        PrintWriter pw = new PrintWriter(new File("whitelist-gen.json"));
+        File file = new File("whitelist-gen-" + LocalDate.now() + ".json");
+        PrintWriter pw = new PrintWriter(file);
         pw.print(whitelist);
         pw.close();
     }
